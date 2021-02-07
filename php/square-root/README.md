d75e89bdc519bfeca2e952eb3f3781af01667943
nwkwok <nwkwok@gmail.com>
2021-02-07 11:16:28 -0800

    add question and answer to square root challenge

---
+++ php/square-root/README.md
@@ -0,0 +1,5 @@
+sqrt(9) = 3
+sqrt(16) = 4
+sqrt(4) = 2
+
+make a function that does the logic of sqrt
---
+++ php/square-root/square-root.php
@@ -0,0 +1,10 @@
+<?php
+
+function squareRoot($num)
+{
+    for ($i = 0; $i < $num; $i++) {
+        if ($num % $i == 0 && $i * $i = $num) {
+            return $num;
+        }
+    }
+}
