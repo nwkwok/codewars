0ce3b296de95af6ac487ccff39d065ddd0c79623
nwkwok <nwkwok@gmail.com>
2021-02-15 22:14:05 -0800

    commit consonant value challenge

---
+++ php/consonant-value/consonant-value.php
@@ -0,0 +1,29 @@
+<?php
+
+function solve($s)
+{
+    // your code here
+
+    // Create an alphabet array using range
+
+    // Split the string into an array: $strArr
+
+    // Replace vowels from the array
+    // Loop through string array
+    // If we find values that are not vowels
+    // Replace the value with ' '
+    // $strArr[$i] = ' ';
+
+    // Now we have an array with only consonants and spaces
+
+    // Create an array that stores substrings in each index: $subStrArr
+
+    // Grab each index and loop through it
+    // Take this index value    
+    // If != ' ', append to $subStrArr[$i];
+    // break;
+
+    // After this runs, we should get $subStrArr where each index holds consonant substrings.
+
+
+}
