020a68247fd9468e6e2edc9547be73c36b3687ec
nwkwok <nwkwok@gmail.com>
2021-03-09 22:22:30 -0800

    complete challenge

--- php/write-out-numbers/write-out-numbers.php
+++ php/write-out-numbers/write-out-numbers.php
@@ -1,6 +1,6 @@
 <?php
 
-function number2words(int $n)
+function number2words(int $n): string
 {
     $oneToTwenty = array(
         0 => 'zero',
@@ -36,75 +36,112 @@ function number2words(int $n)
         9 => 'ninety'
     );
 
-    print_r($oneToTwenty);
-    print_r($twentyToNinety);
     $output = '';
     $revInt = strrev(strval($n));
 
     // Check if # in hundred thousand
     if (isset($revInt[5])) {
-        if ($revInt[5] == 0 or !empty($revInt[5])) {
+        // return n hundred thousand
+        if ($revInt[5] > 0 and $revInt[4] == 0 and $revInt[3] == 0) {
             $hundredThousand = $oneToTwenty[$revInt[5]];
-            $output .= $hundredThousand . ' ' . 'hundred ';
+            $output .= $hundredThousand . ' hundred thousand ';
+        }
+        if ($revInt[5] > 0 and !isset($hundredThousand)) {
+            $hundredThousand = $oneToTwenty[$revInt[5]];
+            $output .= $hundredThousand . ' hundred ';
         }
     }
 
     // Check if # in ten thousand
     if (isset($revInt[4])) {
+        // return nn thousand
+        if ($revInt[4] == 1 and $revInt[3] == 0 and $revInt[2] == 0 and $revInt[1] == 0 and $revInt[0] == 0) {
+            $tenThousand = 'ten thousand';
+            $output .= $tenThousand;
+            return $output;
+        }
+        // return nn thousand where nn > 19
+        if ($revInt[4] > 1 and $revInt[3] == 0 and $revInt[2] == 0 and $revInt[1] == 0 and $revInt[0] == 0) {
+            $tenThousand = $twentyToNinety[$revInt[4]];
+            $output .= $tenThousand . 'thousand';
+            return $output;
+        }
+        if ($revInt[4] == 1 and $revInt[3] > 0) {
+            $tenThousand = $oneToTwenty[$revInt[4] . $revInt[3]];
+            $output .= $tenThousand . ' thousand ';
+        }
+        if ($revInt[4] == 1 and $revInt[3] == 0) {
+            $tenThousand = $oneToTwenty[$revInt[4] . $revInt[3]];
+            $output .= $tenThousand . ' thousand ';
+        }
+
         // Hypenated # check > 20 and ones digit is > 0
         if ($revInt[4] > 1 and $revInt[3] > 0) {
             $tenThousand = $twentyToNinety[$revInt[4]] . '-' . $oneToTwenty[$revInt[3]];
-            $output .= $tenThousand . ' ' . 'thousand ';
+            $output .= $tenThousand . ' thousand ';
         }
-        if ($revInt[4] == 1) {
-            $tenThousand = $oneToTwenty[$revInt[4]] . ' ' . $oneToTwenty[$revInt[3]];
-            $output .= $tenThousand . ' ' . 'thousand ';
+        if ($revInt[4] > 1 and $revInt[3] == 0) {
+            $tenThousand = $twentyToNinety[$revInt[4]];
+            $output .= $tenThousand . ' thousand ';
         }
     }
 
     // Check if # in thousand
-    if (isset($revInt[3]) and !isset($tenThousand)) {
-        if ($revInt[3] == 0 or !empty($revInt[3])) {
+    if (isset($revInt[3])) {
+        // Return n thousand
+        if ($revInt[3] > 0 and $revInt[2] == 0 and $revInt[1] == 0 and $revInt[0] == 0) {
             $oneThousand = $oneToTwenty[$revInt[3]];
-            $output .= $oneThousand . ' ' . 'thousand ';
+            $output .= $oneThousand . ' thousand';
+            return $output;
+        }
+
+        if ($revInt[3] > 0 and !isset($tenThousand)) {
+            $oneThousand = $oneToTwenty[$revInt[3]];
+            $output .= $oneThousand . ' thousand ';
         }
     }
 
     // Check hundreds
     if (isset($revInt[2])) {
-        if ($revInt[2] == 0 or !empty($revInt[2])) {
+        if ($revInt[2] > 0 and $revInt[1] == 0 and $revInt[0] == 0) {
+            $oneHundred = $oneToTwenty[$revInt[2]];
+            $output .= $oneHundred . ' hundred';
+            return $output;
+        }
+        if ($revInt[2] > 0) {
             $oneHundred = $oneToTwenty[$revInt[2]];
             $output .= $oneHundred . ' ' . 'hundred ';
         }
     }
-
     // Check tens
     if (isset($revInt[1])) {
-        // Hypenated # check > 20 and ones digit is > 0
-        if ($revInt[1] > 1 and $revInt[0] > 0) {
-            $ten = $twentyToNinety[$revInt[1]] . '-' . $oneToTwenty[$revInt[0]];
+        if ($revInt[1] > 1 and $revInt[0] == 0) {
+            $ten = $twentyToNinety[$revInt[1]];
             $output .= $ten;
         }
         if ($revInt[1] == 1 and $revInt[0] == 0) {
             $ten = 'ten';
             $output .= $ten;
+            return $output;
         }
-
         if ($revInt[1] == 1 and $revInt[0] > 0) {
-            $ten = $oneToTwenty[$revInt[1]] . ' ' . $oneToTwenty[$revInt[0]];
+            $ten = $oneToTwenty[$revInt[1] . $revInt[0]];
             $output .= $ten;
         }
-        if ($revInt[1] == 1 and $revInt[0] == 0) {
-            $ten = $oneToTwenty[$revInt[1]] . ' ' . $oneToTwenty[$revInt[0]];
+
+        // Hypenated # check > 20 and ones digit is > 0
+        if ($revInt[1] > 1 and $revInt[0] > 0) {
+            $ten = $twentyToNinety[$revInt[1]] . '-' . $oneToTwenty[$revInt[0]];
             $output .= $ten;
         }
     }
+
+    // Check ones
     if (isset($revInt[0]) and !isset($ten)) {
         if ($revInt[0] == 0 or !empty($revInt[0])) {
             $one = $oneToTwenty[$revInt[0]];
             $output .= $one;
         }
     }
-
     return $output;
 }
