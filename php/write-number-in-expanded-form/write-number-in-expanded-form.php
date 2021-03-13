5633aa4cc53b658aa154be7f5fade8ff70ce5dbb
nwkwok <nwkwok@gmail.com>
2021-03-12 23:41:03 -0800

    submit answer need to return value if indeces following current index is 0

--- php/write-number-in-expanded-form/write-number-in-expanded-form.php
+++ php/write-number-in-expanded-form/write-number-in-expanded-form.php
@@ -1,5 +1,24 @@
 <?php
-function expanded_form(int $n)
+function expanded_form(int $n): string
 {
-    // Your code here
+    $revNum = strrev($n);
+    $strArr = array();
+    $output = '';
+
+    for ($i = 0; $i < strlen($revNum); $i++) {
+        $strArr[] = $revNum[$i] . str_repeat('0', $i);
+    }
+
+    for ($j = count($strArr) - 1; $j >= 0; $j--) {
+        if ($j == 0) {
+            $output .= $strArr[$j];
+        } elseif ($strArr[$j] == 0) {
+            continue;
+        } else {
+            $output .= $strArr[$j] . ' + ';
+        }
+    }
+
+
+    return $output;
 }
